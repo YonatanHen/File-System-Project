6bdbc7095d9784ad23a9cf80a876f147d901d0f4
YehonatanHen <yonatan.h333@gmail.com>
2025-01-15 22:18:36 +0200

    fixed constructors

--- src/model/Directory.java
+++ src/model/Directory.java
@@ -7,7 +7,6 @@ import java.util.Date;
  * A class that represents a Directory in the system.
  */
 public class Directory extends Item {
-    private String name;
     private ArrayList<Item> items;
 
     /**
@@ -15,8 +14,8 @@ public class Directory extends Item {
      *
      * @param name The name of the directory, up to 32 characters.
      */
-    public Directory (String name) {
-        super(name);
+    public Directory (String name, Date creationDate) {
+        super(name, creationDate);
         this.items = new ArrayList<Item>();
     }
 
