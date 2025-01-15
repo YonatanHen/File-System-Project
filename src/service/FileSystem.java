c41bdf224d13529af3c039a9d173e203e8f6d705
YehonatanHen <yonatan.h333@gmail.com>
2025-01-15 20:06:12 +0200

    added a BFS-like function for finding items

--- src/service/FileSystem.java
+++ src/service/FileSystem.java
@@ -1,6 +1,12 @@
 package service;
 
+import model.Directory;
+import model.File;
+import model.Item;
 import model.RootDirectory;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * The file system service which includes the implementation of the requirements.
@@ -37,5 +43,33 @@ public class FileSystem {
 
     public void delete(String name) {
 
+    }
+
+    /**
+     * This function runs a BFS algorithm that finds a file/directory in the system.
+     * If not such item exists, returns null.
+     *
+     * @param itemName The name of the searched item.
+     * @return An Item object if exists, otherwise null.
+     */
+    public Item FindItem(String itemName) {
+        Queue<Item> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            // Retrieve the item at the front of the Q and removes it.
+            Item current = q.poll();
+
+            if (current.getName().equals(itemName)) {
+                return current;
+            }
+
+            if (current instanceof Directory dir) {
+                // Enqueue all subdirectories and files of the current directory.
+                q.addAll(dir.getDictionaryItems());
+            }
+        }
+
+        return null;
     }
 }
